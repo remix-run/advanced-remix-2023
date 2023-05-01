41d4bcfce3d48f692ca38f0b143a6ca8830ea61e
Ryan Florence <rpflorence@gmail.com>
2023-05-01 14:56:52 -0600

    remove old code

--- 3-server-caching/old/loaders/ghFetch.js
+++
@@ -1,5 +0,0 @@
-module.exports = (url, init = {}) => {
-  init.headers = init.headers || {};
-  init.headers["authorization"] = `token ${process.env.GH}`;
-  return fetch(url, init);
-};
--- 3-server-caching/old/loaders/routes/$org.$repo.js
+++
@@ -1,9 +0,0 @@
-const fetch = require("../ghFetch");
-
-module.exports = async ({ params }) => {
-  let [releases, repo] = await Promise.all([
-    fetch(`https://api.github.com/repos/${params.org}/${params.repo}/releases`),
-    fetch(`https://api.github.com/repos/${params.org}/${params.repo}`),
-  ]);
-  return { releases: await releases.json(), repo: await repo.json() };
-};
