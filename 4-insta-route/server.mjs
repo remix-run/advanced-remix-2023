b78581d1c13ea6c1162079879ac9e68a7f2ac0c0
Ryan Florence <rpflorence@gmail.com>
2023-05-01 14:55:50 -0600

    update deps

--- 4-insta-route/server.mjs
+++ 4-insta-route/server.mjs
@@ -1,8 +1,10 @@
 import express from "express";
 import { createRequestHandler } from "@remix-run/express";
+import { broadcastDevReady } from "@remix-run/node";
 import build from "./build/index.js";
 import getPort from "get-port";
 import path from "path";
+import readline from "readline";
 
 const app = express();
 app.use(express.static("public/build", { immutable: true, maxAge: "365d" }));
@@ -14,9 +16,16 @@ let port = await getPort({ port: process.env.PORT || 3000 });
 
 app.listen(port, () => {
   let name = path.basename(new URL(".", import.meta.url).pathname);
-  console.clear();
+  broadcastDevReady(build);
+  clearConsole();
   console.log(`Running exercise "${name}"`);
   console.log("");
   console.log(`👉 http://localhost:${port}`);
   console.log("");
 });
+
+function clearConsole() {
+  console.log("\n".repeat(process.stdout.rows));
+  readline.cursorTo(process.stdout, 0, 0);
+  readline.clearScreenDown(process.stdout);
+}
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+TODO: YOU ARE HERE. Write some nice instructions here
+
 ```sh
 # from the root of this repository
 npm i
